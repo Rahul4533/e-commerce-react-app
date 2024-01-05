da0de241d0905546bb81b24f229f0636ab6b3567
Rahul <Rahulsingh4533@gmail.com>
2024-01-06 00:03:00 +0530

    design navBar for App

---
+++ src/comonents/NavBar/navbar.js
@@ -0,0 +1,21 @@
+import "./navbar.css";
+export function Navbar() {
+  return (
+    <>
+    <div className="nav-bar">
+      <h4>E-commerce</h4>
+      <h4>Products</h4>
+      <h4>Add A Products</h4>
+      <img  src="https://cdn-icons-png.flaticon.com/128/12484/12484424.png"  alt="Add Button"/>
+      <div className="user">
+      <h3>{"Rahul"}</h3>
+        <img src="https://cdn-icons-png.flaticon.com/128/3135/3135715.png" alt="User" title="User"/>
+        </div>
+    </div>
+
+    
+   
+    
+    </>
+  );
+}
