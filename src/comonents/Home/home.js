48f903bf258ac289cc88ee757b48c78cbbcb069a
Rahul <Rahulsingh4533@gmail.com>
2024-01-10 21:55:26 +0530

    adding new api for products

--- src/comonents/Home/home.js
+++ src/comonents/Home/home.js
@@ -5,34 +5,26 @@ import { useSelector } from "react-redux";
 import "./home.css";
 import StarRating from "./starrating";
 export default function Home() {
- 
   const dispatch = useDispatch();
   const Item = useSelector((state) => state.productsReducer.data);
-  const [searchdata,setSearchdata]=useState("");
+  const [searchdata, setSearchdata] = useState("");
   const [Products, setProducts] = useState([]);
 
-   
+  //Search Function To Filter the Result based on the User Input
 
-   //Search Function To Filter the Result based on the User Input
+  function searchProduct() {
+    const FilterProducts = Item.filter((product) =>
+      product.category.toLowerCase().includes(searchdata.toLowerCase())
+    );
 
-   function searchProduct() {
-  const FilterProducts=  Item.filter((product) =>
-    product.category.toLowerCase().includes(searchdata.toLowerCase())
-  );
-    
-     setProducts(FilterProducts);
-   }
-
-   
-
-
-
+    setProducts(FilterProducts);
+  }
 
   useEffect(() => {
     // fetch data
     async function FetchData() {
       try {
-        const response = await fetch("https://fakestoreapi.com/products");
+        const response = await fetch("https://dummyjson.com/products");
         if (!response.ok) {
           throw new Error("Not Abel to fetch the data !!");
         }
@@ -44,32 +36,38 @@ export default function Home() {
       }
     }
 
-    FetchData().then((data)=>{setProducts(data)});
+    FetchData().then((data) => {
+     
+      setProducts(data.products);
+    });
   }, [searchdata]);
   dispatch(productsActions.products(Products));
-  console.log(Item);
+   console.log(Products);
   return (
     <>
       <div className="search">
-       <h1><input type="text" placeholder="Search"
-        value={searchdata} onChange={(e)=>setSearchdata(e.target.value)}
-        
-        />
-         <button onClick={searchProduct}>Search</button></h1>
-       
+        <h1>
+          <input
+            type="text"
+            placeholder="Search"
+            value={searchdata}
+            onChange={(e) => setSearchdata(e.target.value)}
+          />
+          <button onClick={searchProduct}>Search</button>
+        </h1>
       </div>
       <div>
         {Item.map((products, i) => {
           return (
             <div className="container" key={i}>
               <div className="box">
-                <img src={products.image} alt="products" />
+                <img src={products.images[0]} alt="products" />
 
                 <div className=" Title">
                   <h3>{products.title}</h3>
                   <h1>{"\u20B9" + products.price}</h1>
-
-                  <StarRating rating={products.rating.rate} />
+                   <h2>{products.discountPercentage}%OFF</h2>
+                  <StarRating rating={products.rating} />
                 </div>
 
                 <div className="discription">
@@ -88,7 +86,6 @@ export default function Home() {
             </div>
           );
         })}
-        
       </div>
     </>
   );
